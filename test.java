e9df95abdb0086c5302e8ef7a59cfb8688977703
0vanish0 <info@codingplant.com>
2013-08-08 10:29:25 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 public class test{
-  public static void main(){
-      System.out.println("HelloWorld!!!);
+  public static void main(String args[]){
+      System.out.println("HelloWorld!!!");
   }
 }
